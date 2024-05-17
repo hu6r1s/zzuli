33e48174d93d74aef2f0f66a0babed980346552a
Hu6r1s <darmaa333@gmail.com>
2024-05-17 18:28:48 +0900

    init commit

---
+++ src/test/java/com/hu6r1s/zzuli/ZzuliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hu6r1s.zzuli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzuliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
